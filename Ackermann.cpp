e51e4ad981e3fdc3ffe34fe321fc0880c3ef1615
gvazquez2025 <gianni.vazquez11@gmail.com>
2025-10-01 23:23:10 -0700

    modified:   Ackermann.cpp

--- Ackermann.cpp
+++ Ackermann.cpp
@@ -1,9 +1,15 @@
-#include <iostream>
+#include <iostream> 
 using namespace std;
 
 // Ackermann function definition
 unsigned long long ackermann(unsigned int m, unsigned int n) {
- // TODO: Complete the code.
+    if (m == 0) {
+        return n + 1;
+    } else if (m > 0 && n == 0) {
+        return ackermann(m - 1, 1);
+    } else {
+        return ackermann(m - 1, ackermann(m, n - 1));
+    }
 }
 
 int main() {
